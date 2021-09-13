8b1719e639d4b3d890e04a51860b61d668b70b73
Elise Brown <elisemarie88@gmail.com>
2021-09-13 16:51:09 -0600

    icons and table width

--- src/Song.js
+++ src/Song.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Button, Table } from 'semantic-ui-react';
+import { Button, Table, Icon } from 'semantic-ui-react';
 
-const Song = ( {id, name, artist, remove} ) => ( 
+const Song = ( {id, name, artist, remove, update} ) => ( 
   
   
   <Table.Row key={id}>
     <Table.Cell><strong>{name}</strong></Table.Cell>
     <Table.Cell><strong>{artist}</strong></Table.Cell>
     <Table.Cell>
-      <Button size="small" onClick={()=> remove(id)}>
-          Delete
+      <Button color="teal" icon size="small" onClick={()=> update({id,name,artist})}>
+          <Icon name="edit" />
+      </Button>
+      <Button icon size="small" onClick={()=> remove(id)}>
+          <Icon name="trash alternate outline" />
       </Button>
     </Table.Cell>
   </Table.Row>
--- src/Songs.js
+++ src/Songs.js
@@ -3,19 +3,22 @@ import { Button, Table } from 'semantic-ui-react';
 import Song from './Song';
 
 
-const Songs = ( {songs, remove} ) => (
+const Songs = ( {songs, remove, update} ) => (
   <Table celled padded color="purple" inverted>
     <Table.Header>
       <Table.Row>
         <Table.HeaderCell>Song Name</Table.HeaderCell>
         <Table.HeaderCell>Artist</Table.HeaderCell>
-        <Table.HeaderCell>Remove</Table.HeaderCell>
+        <Table.HeaderCell class="three wide">Edit / Remove</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
 
     <Table.Body>
       {songs.map((song) => (
-        <Song {...song} remove={remove}/>
+        <Song 
+          {...song} 
+          remove={remove}
+          update={update}/>
         ))
       }
     </Table.Body>
