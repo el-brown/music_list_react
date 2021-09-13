d2bf64e16f054b6b5427dc6893259271535fe46e
Elise Brown <elisemarie88@gmail.com>
2021-09-13 16:16:18 -0600

    add song works

--- src/SongForm.js
+++ src/SongForm.js
@@ -2,15 +2,36 @@ import React from 'react';
 import {Form , Button, Message } from 'semantic-ui-react';
 
 class SongForm extends React.Component  {
+  state = { name: "", artist: "", };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.add(this.state);
+    this.setState({ name: "", artist: "", })
+  };
+
+  handleChange = (e, {value}) => {
+    this.setState({[e.target.name]: value, });
+  };
 
   render() {
     return (
       <div>
         <h3>Add a song</h3>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group widths='equal'>
-            <Form.Input fluid label='Song' placeholder='song' />
-            <Form.Input fluid label='Artist' placeholder='artist' />
+            <Form.Input 
+              fluid label='Song' 
+              placeholder='song'
+              name = "name"
+              value = {this.state.name}
+              onChange = {this.handleChange}/>
+            <Form.Input 
+              fluid label='Artist' 
+              placeholder='artist' 
+              name = "artist"
+              value = {this.state.artist}
+              onChange = {this.handleChange}/>
           </Form.Group>
           <Button size="small">Submit</Button>
         </Form>
